fbeca0605a62118f5f17712cb3216d67ba722d37
Jessica Sutherland <me@jessicasuther.land>
2020-10-31 08:43:27 +1100

    Sidebars experiment

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,18 @@
 module.exports = {
+<<<<<<< Updated upstream
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
+=======
+  Docs: {
+    Welcome: ['welcome'],
+    Billing: ['cancel-account'],
+    Features: ['uptime-monitor'],
+    Support: ['my-site-is-down', 'my-site-is-slow'],
+  },
+
+  Policy: {
+    Policy: ['acceptable-use'],
+>>>>>>> Stashed changes
   },
 };
