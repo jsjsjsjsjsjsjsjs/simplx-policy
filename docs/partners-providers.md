28490a40dfd0ae8df504a954d97363f019563941
Jessica Sutherland <me@jessicasuther.land>
2020-10-31 15:35:33 +1100

    Added partners article

---
+++ docs/partners-providers.md
@@ -0,0 +1,7 @@
+---
+id: partners-providers
+title: Downstream providers and business partners
+sidebar_label: Downstream providers and business partners
+---
+
+![Google Cloud Logo](https://www.gstatic.com/devrel-devsite/prod/vf2803d8fceba443283ee4e8627acfcc1365957a4f42d24f2965d2cb7faab19ba/cloud/images/cloud-logo.svg?dcb_=0.7686924662220247)
--- sidebars.js
+++ sidebars.js
@@ -3,10 +3,15 @@ module.exports = {
     Welcome: ['welcome'],
     Billing: ['cancel-account'],
     Features: ['uptime-monitor'],
+<<<<<<< Updated upstream
     Support: ['my-site-is-down', 'my-site-is-slow'],
   },
 
   Policy: {
     Policy: ['acceptable-use'],
+=======
+    Support: ['my-site-is-down', 'my-site-is-slow', 'under-attack'],
+    Policy: ['policy-centre', 'acceptable-use', 'partners-providers'],
+>>>>>>> Stashed changes
   },
 };
