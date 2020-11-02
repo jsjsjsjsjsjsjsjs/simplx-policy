c738352ae937e8271f2a87828754c955228e9882
Jessica Sutherland <me@jessicasuther.land>
2020-11-02 17:51:16 +1100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
     Billing: ['cancel-account'],
     Features: ['uptime-monitor', 'cdn'],
     Support: ['my-site-is-down', 'my-site-is-slow', 'under-attack'],
-    Policy: ['policy-centre', 'acceptable-use', 'partners-providers', 'uptime-sla', 'renewals'],
+    Policy: ['policy-centre', 'acceptable-use', 'partners-providers', 'uptime-sla'],
     DSCM: ['orion', 'dscm-login'],
   },
 };
