18fb9c7a34858c5b017ddccb858d3df26c1e05ce
Jessica Sutherland <me@jessicasuther.land>
2020-11-07 23:35:51 +1100

    Added renewals

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,10 @@
 module.exports = {
   Docs: {
     Welcome: ['welcome'],
-    Billing: ['cancel-account'],
+    Billing: ['cancel-account', 'renewals'],
     Features: ['uptime-monitor', 'cdn'],
     Support: ['my-site-is-down', 'my-site-is-slow', 'under-attack'],
-    Policy: ['policy-centre', 'acceptable-use', 'partners-providers', 'uptime-sla'],
+    Policy: ['policy-centre', 'acceptable-use', 'partners-providers', 'uptime-sla', 'renewals'],
     DSCM: ['orion', 'dscm-login'],
   },
 };
